579f8d467928d69d6c8225a95fc3223f5b6abd8b
maximfreerider <ivanovma41@gmail.com>
2019-12-10 19:13:07 +0200

    'build'

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM httpd:2.4
-COPY ./build/ /usr/local/apache2/htdocs/
+COPY . /usr/local/apache2/htdocs/
---
+++ build/precache-manifest.0941723d823c8fcd8ab4399f28e15807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e434f5fa5cf5329ddc05a644e345e8a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69d94af466600e4d2ff3",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f631c5dcca1cfa2bc72a",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "69d94af466600e4d2ff3",
+    "url": "/static/js/2.93490aa8.chunk.js"
+  },
+  {
+    "revision": "f631c5dcca1cfa2bc72a",
+    "url": "/static/js/main.9c0ee1fe.chunk.js"
+  },
+  {
+    "revision": "47e1767f5bee9d6b47cb",
+    "url": "/static/js/runtime-main.2967b1c1.js"
+  }
+]);
